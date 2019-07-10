13e10c0c8902eb0889ab07c7765158ef47dd964f
VINCENT HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGVI7@OOCL.COM>
2019-07-10 14:49:55 +0800

    finish choose_even and choose_multiples_of_three

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
-function choose_even(collection) {
+// function choose_even(collection) {
+//
+//   //implement here
+//
+// }
 
-  //implement here
+const choose_even = (number) => {
+  const condition = (number) =>{return number % 2 === 0};
+  return number.filter(condition);
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,13 @@
 'use strict';
 
-function choose_multiples_of_three(collection) {
+// function choose_multiples_of_three(collection) {
+//
+//   //implement here
+// }
 
-  //implement here
+const choose_multiples_of_three = (number){
+  const condition = (number) =>{return number % 3 === 0};
+  return number.filter(condition);
 }
 
 module.exports = choose_multiples_of_three;
